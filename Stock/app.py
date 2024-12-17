dd2d7d2e5fbd32a2826d504512586827e0ecee92
Buraisu Tieu <143369585+Buraisu-tiu@users.noreply.github.com>
2024-12-16 18:20:21 -0800

    online

--- Stock/app.py
+++ Stock/app.py
@@ -5,8 +5,10 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
-#postgresql://avnadmin:AVNS_OMqgHisQpHWFIGV-Vzz@baller-stock-sim.f.aivencloud.com:15808/defaultdb?sslmode=require
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", 
+    "postgresql://xiao:ZZJsF5kz9ARTOZA1KNW34qBRCNWWyt62@dpg-ctg9ljrtq21c7391bdq0-a.oregon-postgres.render.com/stocksim_5hrx"
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
     'pool_size': 10,
